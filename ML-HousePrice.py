f3929600900d78a32656afab4edeba88137edcb5
YuJaeyoung <72694885+YuJaeyoung07@users.noreply.github.com>
2020-11-02 23:52:36 +0900

    Update ML-HousePrice.py

--- ML-HousePrice.py
+++ ML-HousePrice.py
@@ -4,19 +4,19 @@ import seaborn as sns
 import matplotlib
 from sklearn.linear_model import Ridge, RidgeCV, ElasticNet, LassoCV, LassoLarsCV
 from sklearn.model_selection import cross_val_score
-
 import matplotlib.pyplot as plt
 from scipy.stats import skew
 from scipy.stats.stats import pearsonr
-
-
-%config InlineBackend.figure_format = 'retina' #set 'png' here when working on notebook
+%config InlineBackend.figure_format = 'retina' 
 %matplotlib inline
 
+# train 데이터와 test 데이터 읽어들임
 train = pd.read_csv("./train.csv")
 test = pd.read_csv("./test.csv")
 
+# train 데이터에서 상위 5개 행을 불러들임
 train.head()
+
 
 all_data = pd.concat((train.loc[:,'MSSubClass':'SaleCondition'], test.loc[:,'MSSubClass':'SaleCondition']))
 train["SalePrice"] = np.log1p(train["SalePrice"])
@@ -35,19 +35,18 @@ X_train = all_data[:train.shape[0]]
 X_test = all_data[train.shape[0]:]
 y = train.SalePrice
 
+# rmse(root mean squared error) 정의 함수: 맨 앞에 -를 붙여 보정)
 def rmse_cv(model):
     rmse= np.sqrt(-cross_val_score(model, X_train, y, scoring="neg_mean_squared_error", cv = 5))
     return(rmse)
 
+# Ridge 객체 생성 및 alpha값 설정
 model_ridge = Ridge()
 alphas = [0.05, 0.1, 0.3, 1, 3, 5, 10, 15, 30, 50, 75]
 cv_ridge = [rmse_cv(Ridge(alpha = alpha)).mean() for alpha in alphas]
-
 cv_ridge = pd.Series(cv_ridge, index = alphas)
-cv_ridge.plot(title = "Validation - Just Do It")
-plt.xlabel("alpha")
-plt.ylabel("rmse")
 
+# Lasso 객체 생성 및 alpha값 설정
 model_lasso = LassoCV(alphas = [1, 0.1, 0.001, 0.0005]).fit(X_train, y)
 coef = pd.Series(model_lasso.coef_, index = X_train.columns)
 print("Lasso picked " + str(sum(coef != 0)) + " variables and eliminated the other " +  str(sum(coef == 0)) + " variables")
